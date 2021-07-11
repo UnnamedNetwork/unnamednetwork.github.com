a3879eeae0a3a84480f218b3cdf1ca8633c95358
Cuong Tien Dinh <deptteam.cuong@gmail.com>
2021-07-11 22:51:21 +0700

    add support for Windows 11, and more...
    ...changes to json version control, rewritten everything!

--- UNWDS/scripts/installer.ps1
+++ UNWDS/scripts/installer.ps1
@@ -1,173 +1,80 @@
 #Requires -Version 5.1
-# Require PowerShell v5.1 or above to prevent errors. Windows 10/Server version 1607 or newer have already pre-installed.
 
-# Windows 11 removed Internet Explorer, so we current working on alternative plan.
+#TODO: work at 'function as parameter'
 
 # HOW UNWDS INSTALLER (WINDOWS) WORK?
 # UNWDS Installer are based on PowerShell script.
-# First, the installer download a file named "remote_version.info" contains some version strings that helps some update tasks easier.
-# Based on that file, installer have some info for builds they are working with and define it to current version, file named "current_version.info"
+# First, the installer download a file named "remoteVersion.json" contains some version strings that helps some update tasks easier.
+# Based on that file, installer have some info for builds they are working with and define it to current version, file named "currentVersion.json"
 # Then the installer download the server software from Github and PHP from pmmp's Jenkins servers
 # HOW UPDATE WORK?
 
-# When update, the installer compare strings from server and current (so that changing contents in "current_version.info" are not recommended.)
+# When update, the installer compare strings from server and current (so that changing contents in "currentVersion.json" are not recommended.)
 # If the installer detect the new version, it's will download from Github (for server software) or pmmp's Jenkins (for PHP) automatically.
-# Then, the installer delete "current_version.info" file and rename "remote_version.info" to "current_version.info" 
+# Then, the installer delete "currentVersion.json" file and rename "remoteVersion.json" to "currentVersion.json" 
 # Update completed!
 #
-# Enjoy your installer and don't delete current_version.info. Thanks for using this tool.
-# exit()
+# Enjoy your installer and don't delete currentVersion.json. This will trigger cleanup and delete ALL FILE in working folder.
+# Thanks for using this tool.
 
-$scriptVersion = "1.3.0"
+$scriptVersion = "2.0.0"
 $host.ui.RawUI.WindowTitle = "UNWDS Installer (v$scriptVersion)"
-$ProgressPreference = 'SilentlyContinue'
 
 # First, set PS's security protocol to TLS1.2 to avoid Github Releases download problems.
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-# Prepare variables
-$repoName = "UnnamedNetwork/UNWDS"
-$assetPattern = "UNWDS.phar"
-$releasesUri = "https://api.github.com/repos/$repoName/releases/latest"
-$asset = (Invoke-WebRequest $releasesUri | ConvertFrom-Json).assets | Where-Object name -like $assetPattern
-$downloadUri = $asset.browser_download_url
-$CurrentVPath = "$PSScriptRoot\current_version.info"
-$CurrentRPath = "$PSScriptRoot\remote_version.info"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UnnamedNetwork/unnamednetwork.github.io/main/UNWDS/version_control/remote_version.info" -OutFile "$PSScriptRoot\remote_version.info"
+$repo = "UnnamedNetwork/UNWDS"
+$file = "UNWDS.phar"
+$CurrentVPath = "$PSScriptRoot\currentVersion.json"
+$CurrentRPath = "$PSScriptRoot\remoteVersion.json"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UnnamedNetwork/unnamednetwork.github.io/main/UNWDS/version_control/remoteVersion.json" -OutFile "remoteVersion.json"
 
 function GetServerVersion {
-    Write-Output "[*] Contacting updater server to get version..."
-    $Rfile = Get-Content "$CurrentRPath"
-    $RcontainsWord = $Rfile | ForEach-Object{$_ -match "REMOTE_UNWDS_VERSION"}
-    if ($RcontainsWord -contains $true) {
-    } else {
-        Write-Output "[*] Can't found server latest current version."
-    }
-    
-    $Rfile = Get-Content "$CurrentRPath"
-    $RcontainsWord = $Rfile | ForEach-Object{$_ -match "REMOTE_PHP_VERSION"}
-    if ($RcontainsWord -contains $true) {
-    } else {
-        Write-Output "[*] Can't found defined PHP current version."
-    }
-
-    $file = Get-Content "$CurrentRPath"
-    $containsWord = $file | ForEach-Object{$_ -match "REMOTE_RELEASED"}
-    if ($containsWord -contains $true) {
-    } else {
-        Write-Output "[*] Can't found defined released date"
-        $global:remoteReleasedDate = "0"
-    }
-
-    $values = Get-Content $CurrentRPath | Out-String | ConvertFrom-StringData
-    $global:remotePHPversion = $values.REMOTE_PHP_VERSION
-    $global:remoteUNWDSversion = $values.REMOTE_UNWDS_VERSION
-    $global:remoteReleasedDate = $values.REMOTE_RELEASED
-
+    Write-Output "[*] Contacting update server to get version..."
+    Write-Output "`n"
+    $remoteJsonData = Get-Content $CurrentRPath | ConvertFrom-Json
+    $Global:remoteJsonVersion= $remoteJsonData[0].Value
+    $Global:remoteTarget= $remoteJsonData[1].Value
+    $Global:remoteUNWDSversion = $remoteJsonData[2].Value
+    $Global:remoteBuildNumber = $remoteJsonData[3].Value
+    $Global:remotePHPversion = $remoteJsonData[4].Value
+    $Global:remoteReleasedDate = $remoteJsonData[5].Value
+    Write-Output "[*] Server JSON configuration version: $remoteJsonVersion"
     Write-Output "[*] Server latest PHP version is: $remotePHPversion"
-    Write-Output "[*] Server latest UNWDS version is: $remoteUNWDSversion, released in $remoteReleasedDate "
+    Write-Output "[*] Server latest UNWDS version is: $remoteUNWDSversion - $remoteBuildNumber ($remoteTarget), released in $remoteReleasedDate "
     Write-Output "`n"
 }
 
-function UpdateVersionFile {
-    Remove-Item $CurrentVPath -Force -erroraction 'silentlycontinue'
-    Rename-Item -Path $CurrentRPath -NewName "current_version.info"
-    $filePath = $CurrentVPath
-    $tempFilePath = "$env:TEMP\$($filePath | Split-Path -Leaf)"
-    $find = 'REMOTE'
-    $replace = 'CURRENT'
-
-    (Get-Content -Path $filePath) -replace $find, $replace | Add-Content -Path $tempFilePath
-    Get-ChildItem -path "$CurrentVPath" -force | ForEach-Object {$_.Attributes = "Normal"}
-    Remove-Item -Path $filePath
-    Move-Item -Path $tempFilePath -Destination $filePath
-    Get-ChildItem -path "$filePath" -force | ForEach-Object {$_.Attributes = "Hidden"}
+function GetCurrentVersion {
+    $currentJsonData = Get-Content $CurrentVPath | ConvertFrom-Json
+    $Global:currentJsonVersion = $currentJsonData[0].Value
+    $Global:currentTarget = $currentJsonData[1].Value
+    $Global:currentUNWDSversion = $currentJsonData[2].Value
+    $Global:currentBuildNumber = $currentJsonData[3].Value
+    $Global:currentPHPversion = $currentJsonData[4].Value
+    $Global:currentReleasedDate = $currentJsonData[5].Value
+    Write-Output "[*] Current JSON configuraton version: $currentJsonVersion"
+    Write-Output "[*] Current PHP version is: $currentPHPversion"
+    Write-Output "[*] Current UNWDS version is: $currentUNWDSversion - $currentBuildNumber ($currentTarget), released in $currentReleasedDate"
+    Write-Output "`n"
 }
 
-function UpdateChecker{
-    function GetCurrentVersion {
-        $file = Get-Content "$CurrentVPath"
-        $containsWord = $file | ForEach-Object{$_ -match "CURRENT_UNWDS_VERSION"}
-        if ($containsWord -contains $true) {
-        } else {
-            Write-Output "[*] Can't found current version. But still continue to update the server software."
-            $global:currentUNWDSversion = "0"
-        }
-        
-        $file = Get-Content "$CurrentVPath"
-        $containsWord = $file | ForEach-Object{$_ -match "CURRENT_PHP_VERSION"}
-        if ($containsWord -contains $true) {
-        } else {
-            Write-Output "[*] Can't found defined PHP current version. But still continue to update PHP binary."
-            $global:currentPHPversion = "0"
-        }
-
-        $file = Get-Content "$CurrentVPath"
-        $containsWord = $file | ForEach-Object{$_ -match "RELEASED"}
-        if ($containsWord -contains $true) {
-        } else {
-            Write-Output "[*] Can't found defined released date"
-            $global:currentReleasedDate = "0"
-        }
-
-        $values = Get-Content $CurrentVPath | Out-String | ConvertFrom-StringData
-        $global:currentPHPversion = $values.CURRENT_PHP_VERSION
-        $global:currentUNWDSversion = $values.CURRENT_UNWDS_VERSION
-        $global:currentReleasedDate = $values.CURRENT_RELEASED
-
-        Write-Output "[*] Current PHP version is: $currentPHPversion"
-        Write-Output "[*] Current UNWDS version is: $currentUNWDSversion, released in $currentReleasedDate"
+function CompareVersion {
+    if ($remotePHPversion -le $currentPHPversion) {
+        Write-Output "[*] PHP already up-to-date (Server: $remotePHPversion - You have: $currentPHPversion)"
+    } else {
+        Write-Output "[*] PHP need update (Server: $remotePHPversion - You have: $currentPHPversion)"
+        UpdatePHP
     }
- 
-    function UpdatePHP {
-        Remove-Item PHP-$currentPHPversion-Windows-x64 -Recurse -Force -erroraction 'SilentlyContinue'
-        Remove-Item PHP-$remotePHPversion-Windows-x64 -Recurse -Force -erroraction 'silentlycontinue'
-        Remove-Item bin -Recurse -Force -erroraction 'silentlycontinue'
-        Remove-Item PHP-$remotePHPversion-Windows-x64.zip -Force -erroraction 'silentlycontinue'
-        Remove-Item PHP-$currentPHPversion-Windows-x64.zip -Force -erroraction 'silentlycontinue'
-        Remove-Item vc_redist.x64.exe -erroraction 'silentlycontinue'
-        Write-Output "[*] Updating PHP v$remotePHPversion (Windows x64)...";
-        Invoke-WebRequest -Uri "https://jenkins.pmmp.io/job/PHP-$remotePHPversion-Aggregate/lastSuccessfulBuild/artifact/PHP-$remotePHPversion-Windows-x64.zip" -OutFile "PHP-$remotePHPversion-Windows-x64.zip"
-        Expand-Archive -LiteralPath PHP-$remotePHPversion-Windows-x64.zip -Force
-        Get-ChildItem -Path "PHP-$remotePHPversion-Windows-x64" -Recurse |  Move-Item -Destination .
-        Remove-Item PHP-$remotePHPversion-Windows-x64.zip -Force -erroraction 'silentlycontinue'
-        Remove-Item PHP-$remotePHPversion-Windows-x64 -Recurse -Force -erroraction 'silentlycontinue'
-        Write-Output "[*] Update successfully!"
+    if ($remoteUNWDSversion -le $currentUNWDSversion) {
+        Write-Output "[*] UNWDS already up-to-date (Server: $remoteUNWDSversion - You have: $currentUNWDSversion)"
+    } else {
+        Write-Output "[*] UNWDS need update (Server: $remoteUNWDSversion, released in $remoteReleasedDate - You have: $currentUNWDSversion, released in $currentReleasedDate)"
+        UpdateUNWDS
     }
-    function UpdateUNWDS {
-        Remove-Item UNWDS.phar -Force -erroraction 'silentlycontinue'
-        Remove-Item start.cmd -erroraction 'silentlycontinue'
-        Write-Output "[*] Updating UNWDS v$remoteUNWDSversion";
-        Invoke-WebRequest -Uri $downloadUri -OutFile "UNWDS.phar"
-        Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UnnamedNetwork/UNWDS/stable/start.cmd" -OutFile "start.cmd"
-        Write-Output "[*] Update successfully!"
+    UpdateVersionFile
     }
-    
-    function CompareVersion {
-        if ($remotePHPversion -le $currentPHPversion) {
-            Write-Output "[*] PHP already up-to-date (Server: $remotePHPversion - You have: $currentPHPversion)"
-        } else {
-            Write-Output "[*] PHP need update (Server: $remotePHPversion - You have: $currentPHPversion)"
-            UpdatePHP
-        }
-        if ($remoteUNWDSversion -le $currentUNWDSversion) {
-            Write-Output "[*] UNWDS already up-to-date (Server: $remoteUNWDSversion - You have: $currentUNWDSversion)"
-        } else {
-            Write-Output "[*] UNWDS need update (Server: $remoteUNWDSversion, released in $remoteReleasedDate - You have: $currentUNWDSversion, released in $currentReleasedDate)"
-            UpdateUNWDS
-        }
-        UpdateVersionFile
-    }
-    Write-Output "[*] Seems UNWDS has been installed on this directory. Checking for update..."
-    GetCurrentVersion
-    Write-Output "`n"
-    GetServerVersion
-    Write-Output "`n"
-    CompareVersion
-    
-}
 
-# Clean up old files
 function CleanUp {
     Write-Output "`n"
     Write-Output "[*] Welcome to UNWDS Installer!";
@@ -196,19 +103,20 @@ function ErrorCleanUp {
     Remove-Item UNWDS.phar -Force -erroraction 'silentlycontinue'
     Remove-Item start.cmd -erroraction 'silentlycontinue'
     Remove-Item vc_redist.x64.exe -erroraction 'silentlycontinue'
-    Remove-Item remote_version.info -Force -erroraction 'silentlycontinue'
-    Remove-Item current_version.info -Force -erroraction 'silentlycontinue'
+    Remove-Item remoteVersion.json -Force -erroraction 'silentlycontinue'
+    Remove-Item currentVersion.json -Force -erroraction 'silentlycontinue'
     Write-Output "Some file does not exist. UNWDS will not run if missing files. Please try to run install script again."
     $host.ui.RawUI.WindowTitle = "Windows PowerShell" #set the window title back to default
 }
 
-function Main {
-    # First, download the server software phar and startup script...
-    Write-Output "[2/3] Downloading UNWDS v$remoteUNWDSversion, released in $remoteReleasedDate...";
-    Invoke-WebRequest -Uri $downloadUri -OutFile "UNWDS.phar"
+function Install {
+    GetServerVersion
+    Write-Output "[2/3] Downloading UNWDS v$remoteUNWDSversion ($remoteTarget), released in $remoteReleasedDate...";
+    $DownloadURL = "https://github.com/$repo/releases/download/v$remoteUNWDSVersion/$file"
+    Invoke-WebRequest $DownloadURL -Out $file
+    Write-Host $DownloadURL
     Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UnnamedNetwork/UNWDS/stable/start.cmd" -OutFile "start.cmd"
     Write-Output "`n"
-    # Second, download PHP and extract it.
     Write-Output "[3/3] Downloading PHP $remotePHPVersion (Windows x64)...";
     Invoke-WebRequest -Uri "https://jenkins.pmmp.io/job/PHP-$remotePHPversion-Aggregate/lastSuccessfulBuild/artifact/PHP-$remotePHPversion-Windows-x64.zip" -OutFile "PHP-$remotePHPversion-Windows-x64.zip"
     Expand-Archive -LiteralPath PHP-$remotePHPversion-Windows-x64.zip -Force
@@ -216,6 +124,37 @@ function Main {
     Remove-Item PHP-$remotePHPversion-Windows-x64.zip -Force -erroraction 'silentlycontinue'
     Remove-Item PHP-$remotePHPversion-Windows-x64 -Recurse -Force -erroraction 'silentlycontinue'
     Write-Output "`n"
+}
+function UpdatePHP {
+    Remove-Item PHP-$currentPHPversion-Windows-x64 -Recurse -Force -erroraction 'SilentlyContinue'
+    Remove-Item PHP-$remotePHPversion-Windows-x64 -Recurse -Force -erroraction 'silentlycontinue'
+    Remove-Item bin -Recurse -Force -erroraction 'silentlycontinue'
+    Remove-Item PHP-$remotePHPversion-Windows-x64.zip -Force -erroraction 'silentlycontinue'
+    Remove-Item PHP-$currentPHPversion-Windows-x64.zip -Force -erroraction 'silentlycontinue'
+    Remove-Item vc_redist.x64.exe -erroraction 'silentlycontinue'
+    Write-Output "[*] Updating PHP v$remotePHPversion (Windows x64)...";
+    Invoke-WebRequest -Uri "https://jenkins.pmmp.io/job/PHP-$remotePHPversion-Aggregate/lastSuccessfulBuild/artifact/PHP-$remotePHPversion-Windows-x64.zip" -OutFile "PHP-$remotePHPversion-Windows-x64.zip"
+    Expand-Archive -LiteralPath PHP-$remotePHPversion-Windows-x64.zip -Force
+    Get-ChildItem -Path "PHP-$remotePHPversion-Windows-x64" -Recurse |  Move-Item -Destination .
+    Remove-Item PHP-$remotePHPversion-Windows-x64.zip -Force -erroraction 'silentlycontinue'
+    Remove-Item PHP-$remotePHPversion-Windows-x64 -Recurse -Force -erroraction 'silentlycontinue'
+    Write-Output "[*] Update successfully!"
+}
+
+function UpdateUNWDS {
+    $DownloadURL = "https://github.com/$repo/releases/download/v$remoteUNWDSVersion/$file"
+    Remove-Item UNWDS.phar -Force -erroraction 'silentlycontinue'
+    Remove-Item start.cmd -erroraction 'silentlycontinue'
+    Write-Output "[*] Updating UNWDS v$remoteUNWDSversion";
+    Invoke-WebRequest $DownloadURL -Out $file
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UnnamedNetwork/UNWDS/stable/start.cmd" -OutFile "start.cmd"
+    Write-Output "[*] Update successfully!"
+}
+
+function UpdateVersionFile {
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UnnamedNetwork/unnamednetwork.github.io/main/UNWDS/version_control/remoteVersion.json" -OutFile "remoteVersion.json"
+    Remove-Item -Path $CurrentVPath -Force -erroraction 'silentlycontinue'
+    Rename-Item -Path $CurrentRPath -NewName "currentVersion.json"
 }
 
 function CheckFiles {
@@ -239,33 +178,36 @@ function CheckFiles {
     Write-Output "[*] If not, you can run vc_redist.x64.exe in your cuurent folder to install it."
 }
 
-if ([Environment]::Is64BitProcess -ne [Environment]::Is64BitOperatingSystem)
-{
-    Write-Output "[ERR] You're running 32-bit system or you're run this UNWDS Installer in PowerShell (x86), these are not supported by UNWDS.";
-    Write-Output "[ERR] Please consider upgrade to 64-bit system or use PowerShell (x64) to run UNWDS Installer.";
-    throw Unsupported current CPU or PowerShell instance (32bit).
-}
-else
-{
-    if ([Environment]::OSVersion.Version.Build -ge 22000) #Build 22000 is the first public Windows 11 build.
-    {
-        Write-Output "[ERR] You're running Windows 11, which removed Internet Explorer.";
-        Write-Output "[ERR] Internet Explorer is mandatory for UNWDS PS Installer.";
-        Write-Output "[ERR] We're working on UNWDS PS Installer for Windows 11.";
-        Write-Output "[ERR] Please use other way to download UNWDS.";
-        throw Unsupported OS.
-    }
-    else
-    {
-    if (-not (Test-Path -Path current_version.info)) {
-        CleanUp
-        GetServerVersion
+function StartS {
+    function PlatformCheck {
+        if ([Environment]::Is64BitProcess -ne [Environment]::Is64BitOperatingSystem)
+        {
+            Write-Output "[ERR] You're running 32-bit system or you're run this UNWDS Installer in PowerShell (x86), these are not supported by UNWDS.";
+            Write-Output "[ERR] Please consider upgrade to 64-bit system or use PowerShell (x64) to run UNWDS Installer.";
+            throw Unsupported current CPU or PowerShell instance (32bit).
+        } 
+        else
+        {
         Main
+        }
+    }
+    PlatformCheck
+}
+function Update {
+    GetCurrentVersion
+    GetServerVersion
+    CompareVersion
+    UpdateVersionFile
+}
+function Main {
+    if (-not (Test-Path -Path currentVersion.json)) {
+        CleanUp
+        Install
         CheckFiles
         UpdateVersionFile
         $host.ui.RawUI.WindowTitle = "Windows PowerShell" #set the window title back to default
     } else {
-        UpdateChecker
-    }
+        Update
     }
 }
+StartS
